88bf248c784d56ce99d92d5053116921ecda1553
TDMwangi <teddymwangi65@gmail.com>
2024-08-31 05:51:40 +0300

    feat: add variable kms_master_key_id

--- modules/variables.tf
+++ modules/variables.tf
@@ -2,3 +2,8 @@ variable "name" {
   description = "The name of the SNS topic"
   type        = string
 }
+
+variable "kms_master_key_id" {
+  description = "The ID of an AWS-managed CMK for Amazon SNS or a custom CMK"
+  type        = string
+}
