c3c3c80d809492a0e6e021ee74a3be190a2ff40f
TDMwangi <teddymwangi65@gmail.com>
2024-09-03 04:49:18 +0300

    feat: add data principals

--- modules/main.tf
+++ modules/main.tf
@@ -3,5 +3,12 @@ data "aws_caller_identity" "current" {}
 data "aws_iam_policy_document" "sns_policy_document" {
   statement {
     effect = "Allow"
+
+    principals {
+      type = "AWS"
+      identifiers = [
+        data.aws_caller_identity.current.arn
+      ]
+    }
   }
 }
