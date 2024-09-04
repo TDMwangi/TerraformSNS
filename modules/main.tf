75569d9627d7f394c21d0710c29700552d84003e
TDMwangi <teddymwangi65@gmail.com>
2024-09-04 08:32:13 +0300

    feat: add data actions

--- modules/main.tf
+++ modules/main.tf
@@ -10,5 +10,8 @@ data "aws_iam_policy_document" "sns_policy_document" {
         data.aws_caller_identity.current.arn
       ]
     }
+
+    actions   = ["SNS:Publish", "SNS:Subscribe"]
+    resources = ["arn:aws:sns:eu-west-1:${data.aws_caller_identity.current.account_id}:${aws_sns_topic.accout_activity_topic.name}"]
   }
 }
